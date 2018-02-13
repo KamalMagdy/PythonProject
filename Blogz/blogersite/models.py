62a52e7bd3f31c4796940cb8e002f1fdab24d071
Minos32x <minasamy65@gmail.com>
2018-02-13 19:36:54 +0200

    edit branch

--- Blogz/blogersite/models.py
+++ Blogz/blogersite/models.py
@@ -1,1 +1,23 @@
 from django.db import models
+
+class User(models.Model):
+    user_name=models.CharField(max_length=20)
+    user_password=models.IntegerField()
+    user_email=models.CharField(max_length=20)
+    is_blocked=models.CharField(max_length=20)
+    is_admin=models.CharField(max_length=20)
+
+    def is_admin_user(self):
+        if self.is_admin=="yes":
+            return True
+        else:
+            return False
+    def is_blocked_user(self):
+       if self.is_blocked =="yes":
+           return True
+       else:
+           return False
+    is_admin_user.boolean=True
+    is_admin_user.short_description='Admin'
+    is_blocked_user.boolean=True
+    is_blocked_user.short_description='Blocked'
